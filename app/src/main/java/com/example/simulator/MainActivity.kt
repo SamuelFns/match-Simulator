43aebe2e538184f6e1645e2c3ed480ba6aead004
TioSam <samuelnunessouza@hotmail.com>
2022-06-23 12:06:08 -0300

    Começando o material design

--- app/src/main/java/com/example/simulator/MainActivity.kt
+++ app/src/main/java/com/example/simulator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //val textId: TextView = findViewById<TextView>(R.id.txtHello)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.txtHello.text = "Olá mizeráveis"
     }
 }
